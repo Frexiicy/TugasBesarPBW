84c5c22ceb9c6c1517bb0f46517f84c1d126d450
unknown <elianalitaniii@gmail.com>
2024-12-11 19:49:45 +0700

    Init commit

---
+++ src/test/java/com/example/rentalfilm/RentalfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalfilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
